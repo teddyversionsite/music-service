ac744ab79e803750fabd57075a83d54a7a99da5c
tylerrasor <trasor@gmail.com>
2024-02-04 13:35:51 -0800

    set up flyway

---
+++ src/main/resources/db/migration/V00001__create_database.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS songs (
+    id SERIAL PRIMARY KEY,
+    display_name VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS shows (
+    id SERIAL PRIMARY KEY,
+    show_date DATE NOT NULL,
+    venue VARCHAR(50) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    state VARCHAR(50),
+    country VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS user_votes (
+    user_id INT, -- figure out how to reference eventual user table/db
+    song_id INT,
+    show_id INT,
+    FOREIGN KEY(song_id) REFERENCES songs(id),
+    FOREIGN KEY(show_id) REFERENCES shows(id)
+);
