68e1a77a2a869d74a2f6cb29b752829b20630f42
tylerrasor <trasor@gmail.com>
2024-02-04 11:36:31 -0800

    spring initilzr

---
+++ src/main/java/com/teddyversion/musicService/MusicServiceApplication.java
@@ -0,0 +1,13 @@
+package com.teddyversion.musicService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicServiceApplication.class, args);
+	}
+
+}
